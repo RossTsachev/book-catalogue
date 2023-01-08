17ef13be6218dbc36cc06e363ac1077b7e191288
RossTsachev <ross102t@gmail.com>
2023-01-08 10:46:23 +0200

    Add flowbite components

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import TomSelect from 'tom-select';
+
+window.TomSelect = TomSelect;
+
+import 'flowbite';
+
+import Datepicker from 'flowbite-datepicker/Datepicker';
+
+window.Datepicker = Datepicker;
